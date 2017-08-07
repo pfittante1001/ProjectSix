0ff4acb1db013bef3c57ea33e1827489771f78cf
pfittante <pfittante@yahoo.com>
2017-08-06 22:06:17 -0400

    staffordPart complete

--- ProjectSix/Controllers/OutfitsController.cs
+++ ProjectSix/Controllers/OutfitsController.cs
@@ -45,6 +45,11 @@ namespace ProjectSix.Controllers
             var outfits = db.Outfits.Include(o => o.Accessory).Include(o => o.Bottom).Include(o => o.Shoe).Include(o => o.Top);
             return View(outfits.ToList());
         }
+        public ActionResult StaffordPart()
+        {
+            var outfits = db.Outfits.Include(o => o.Accessory).Include(o => o.Bottom).Include(o => o.Shoe).Include(o => o.Top);
+            return View(outfits.ToList());
+        }
 
         // GET: Outfits/Details/5
         public ActionResult Details(int? id)
